5d5acc7d3cb6d5da61adf9a15a8bd366e38cc231
Andrus <andrusjakobson@hotmail.com>
2019-07-04 17:49:03 +0300

    Initialize project
    Adding Jquery, bootstrap, index file

---
+++ src/main/java/blackjack/JavaSpringBlackjackApplication.java
@@ -0,0 +1,15 @@
+package blackjack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class JavaSpringBlackjackApplication implements WebMvcConfigurer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBlackjackApplication.class, args);
+	}
+
+}
