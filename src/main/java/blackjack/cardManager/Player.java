750d1dce88c5fd4d9b0ddb637a9a3b8170665451
Andrus <andrusjakobson@hotmail.com>
2019-07-05 21:59:40 +0300

    Updated: Player, Index
    *--Player
    -Added handWorth variable for keeping track of hands worth.
    -Added methods for easier hand worth manipulation
    -Added method for ace detecting and changing.
    *--Index
    -Added a loss screen.

--- src/main/java/blackjack/cardManager/Player.java
+++ src/main/java/blackjack/cardManager/Player.java
@@ -5,25 +5,60 @@ import java.util.ArrayList;
 import blackjack.models.Card;
 
 public class Player {
-	ArrayList<Card> playerHand = new ArrayList<Card>();
+	private ArrayList<Card> playerHand = new ArrayList<Card>();
+	private int handWorth = 0;
 	
+	private void addToHandWorth(int addition) {
+		handWorth+= addition;
+	}
+	
+	private void deductFromHandWorth(int deduction) {
+		handWorth-= deduction;
+	}
+	
+	public void sethandWorth(int handWorth) {
+		this.handWorth = handWorth;
+	}
+	
+	public int getHandWorth() {
+		return handWorth;
+	}
+
 	public ArrayList<Card> getPlayerHand(){
 		return playerHand;
 	}
 	
 	public void addCard(Card card) {
 		playerHand.add(card);
+		addToHandWorth(card.getWorth());
 	}
 	
 	public void addCards(ArrayList<Card> cards) {
-		playerHand.addAll(cards);
+		for(Card card : cards) {
+			addCard(card);
+		}
 	}
 	
-	public int getHandWorth() {
-		int totalWorth = 0;
+	
+	
+	
+	public void lowerAceWorth() {
 		for(Card card : playerHand) {
-			totalWorth+= card.getWorth();
+			if(isHighWorthAce(card)) {
+				card.setWorth(1);
+			}
 		}
-		return totalWorth;
+	}
+	
+	private boolean isAce(Card card) {
+		String cardSuit = card.getSuit();
+		return cardSuit.equals("Ace");
+	}
+	
+	private boolean isHighWorthAce(Card card) {
+		if(isAce(card)) {
+			return card.getWorth() == 14;
+		}
+		return false;
 	}
 }
